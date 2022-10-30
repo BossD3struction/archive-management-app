768c628ba9c7b20c91a68e4b20ef6d4b7da74e31
BossD3struction <BossD3struction@gmail.com>
2022-10-30 02:45:09 +0200

    mp3 file datatable

---
+++ app/DataTables/Mp3FileDataTable.php
@@ -0,0 +1,88 @@
+<?php
+/** @noinspection PhpUnused */
+
+namespace App\DataTables;
+
+use App\Models\Mp3File;
+use Illuminate\Database\Eloquent\Builder;
+use Yajra\DataTables\DataTableAbstract;
+use Yajra\DataTables\Html\Column;
+use Yajra\DataTables\Services\DataTable;
+
+class Mp3FileDataTable extends DataTable
+{
+    /**
+     * Build DataTable class.
+     *
+     * @param mixed $query Results from query() method.
+     * @return DataTableAbstract
+     */
+    public function dataTable($query): DataTableAbstract
+    {
+        return datatables()
+            ->eloquent($query)
+            ->addColumn('action', function ($query) {
+                return '<a href="/files/' . $query->id . '/edit" class="btn btn-primary mb-2" role="button">Edit</a>';
+            });
+    }
+
+    /**
+     * Get query source of dataTable.
+     *
+     * @param Mp3File $model
+     * @return Builder
+     */
+    public function query(Mp3File $model): Builder
+    {
+        return $model->newQuery();
+    }
+
+    /**
+     * Optional method if you want to use html builder.
+     *
+     * @return \Yajra\DataTables\Html\Builder
+     */
+    public function html(): \Yajra\DataTables\Html\Builder
+    {
+        return $this->builder()
+            ->setTableId('test-table')
+            ->columns($this->getColumns())
+            ->minifiedAjax()
+            ->dom('Bfrtip')
+            ->addTableClass('table-striped w-100')
+            ->fixedHeader()
+            ->orderBy(1);
+    }
+
+    /**
+     * Get columns.
+     *
+     * @return array
+     */
+    protected function getColumns(): array
+    {
+        return [
+            Column::make('id'),
+            Column::make('filename_path'),
+            Column::make('filename'),
+            Column::make('title'),
+            Column::make('artist'),
+            Column::make('album'),
+            Column::computed('action')
+                ->exportable(false)
+                ->printable(false)
+                ->width(60)
+                ->addClass('text-center'),
+        ];
+    }
+
+    /**
+     * Get filename for export.
+     *
+     * @return string
+     */
+    protected function filename(): string
+    {
+        return 'Mp3File_' . date('YmdHis');
+    }
+}

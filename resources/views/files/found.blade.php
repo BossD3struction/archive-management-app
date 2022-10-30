aca4a1204385028437ba14d08e1c52c677749f31
BossD3struction <BossD3struction@gmail.com>
2022-10-30 23:07:38 +0100

    refactor, autocomplete=off, css update

--- resources/views/files/found.blade.php
+++ resources/views/files/found.blade.php
@@ -5,16 +5,16 @@
         <form action="/upload/files" method="POST">
             @csrf
             <h1>Found files page</h1>
-            <div>
-                <ul>
+            <div class="found-files-list">
+                <ul class="list-group">
                     @foreach($files as $file)
-                        <li>{{ $file }}</li>
+                        <li class="list-group-item text-center mt-1">{{ $file }}</li>
                         <input type="hidden" name="found_files[]" value="{{$file}}">
                     @endforeach
                 </ul>
             </div>
-            <div class="input-group">
-                <button type="submit" class="btn btn-primary w-25">Upload files</button>
+            <div class="input-group justify-content-center">
+                <button type="submit" class="btn btn-primary w-25 mt-3">Upload files</button>
             </div>
         </form>
     </div>

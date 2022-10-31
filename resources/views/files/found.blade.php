ccadbbff2b74a2707b1d9407d251400d0fc9c7af
BossD3struction <BossD3struction@gmail.com>
2022-10-31 16:10:28 +0100

    - added into view, alert if no .mp3 files found

--- resources/views/files/found.blade.php
+++ resources/views/files/found.blade.php
@@ -2,20 +2,28 @@
 
 @section('content')
     <div class="container">
-        <form action="/upload/files" method="POST">
-            @csrf
+        @if($isEmpty)
             <h1>Found files page</h1>
-            <div class="found-files-list">
-                <ul class="list-group">
-                    @foreach($files as $file)
-                        <li class="list-group-item text-center mt-1">{{ $file }}</li>
-                        <input type="hidden" name="found_files[]" value="{{$file}}">
-                    @endforeach
-                </ul>
+            <div class="alert alert-danger text-center mt-1">
+                <span>No .mp3 files in given directory path found</span>
             </div>
-            <div class="input-group justify-content-center">
-                <button type="submit" class="btn btn-primary w-25 mt-3">Upload files</button>
-            </div>
-        </form>
+        @endif
+        @if(!$isEmpty)
+            <form action="/upload/files" method="POST">
+                @csrf
+                <h1>Found files page</h1>
+                <div class="found-files-list">
+                    <ul class="list-group">
+                        @foreach($files as $file)
+                            <li class="list-group-item text-center mt-1">{{ $file }}</li>
+                            <input type="hidden" name="found_files[]" value="{{$file}}">
+                        @endforeach
+                    </ul>
+                </div>
+                <div class="input-group justify-content-center">
+                    <button type="submit" class="btn btn-primary w-25 mt-3">Upload files</button>
+                </div>
+            </form>
+        @endif
     </div>
 @endsection

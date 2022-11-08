3f00067332c97cdd289d2eec1b713429ac017646
BossD3struction <BossD3struction@gmail.com>
2022-11-09 00:30:14 +0100

    code cleanup/refactor

--- resources/views/files/edit.blade.php
+++ resources/views/files/edit.blade.php
@@ -1,7 +1,6 @@
 @extends('layouts.app')
 
 @section('content')
-    {{--<div class="mt-5 pt-5 pb-5 text-black">--}}
     <div class="text-black">
         <div class="text-center text-uppercase fw-bold">
             <h1>MP3 file</h1>
@@ -12,11 +11,6 @@
         <form action="/files/mp3/{{ $file->id }}" method="POST">
             @csrf
             @method('PUT')
-            {{--<div class="mb-3">
-                <label for="id" class="fw-bold">ID</label>
-                <input type="text" class="form-control form-control-lg" name="id" id="id"
-                       value="{{ $file->id }}" autocomplete="off" readonly>
-            </div>--}}
             <div class="mb-3">
                 <label for="filename_path" class="fw-bold">FILENAME PATH</label>
                 <input type="text" class="form-control form-control-lg" name="filename_path" id="filename_path"
@@ -43,31 +37,22 @@
                        value="{{ $file->album }}" autocomplete="off">
             </div>
             <div class="mb-3">
-                <label for="genre" class="fw-bold">GENRE</label>
-                <select class="form-select form-select-lg" name="genre" id="genre" onchange="">
+                <label for="genres" class="fw-bold">GENRE</label>
+                <select class="form-select form-select-lg" name="genres" id="genres">
                     @foreach($genres as $genre)
                         @if($genre === $file->genre)
-                            <option selected value="{{$genre}}">{{$genre}}</option>
+                            <option selected value="{{ $genre }}">{{ $genre }}</option>
                         @endif
                         @if($genre !== $file->genre)
-                            <option value="{{$genre}}">{{$genre}}</option>
+                            <option value="{{ $genre }}">{{ $genre }}</option>
                         @endif
                     @endforeach
                 </select>
             </div>
-            {{--<div class="mb-3">
-                <label for="year" class="fw-bold">YEAR</label>
-                <input type="date" class="form-control form-control-lg" name="year" id="year"
-                       value="{{ $file->year }}-01-01" autocomplete="off">
-            </div>--}}
             <div class="mb-3">
                 <label for="year" class="fw-bold">YEAR</label>
                 <input type="number" class="form-control form-control-lg" name="year" id="year" placeholder="YYYY"
-                       value="{{ substr($file->year, 0, 4) }}" autocomplete="off" min="1900" max="{{date("Y")}}">
-                {{--<script>
-                    document.querySelector("input[type=number]")
-                        .oninput = e => console.log(new Date(e.target.valueAsNumber, 0, 1));
-                </script>--}}
+                       value="{{ substr($file->year, 0, 4) }}" autocomplete="off" min="1900" max="{{ date("Y") }}">
             </div>
             @if($errors->any())
                 <div class="alert-danger text-center mb-3 p-3">

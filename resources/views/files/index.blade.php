30a055b7de3fb5fbe20f09dec1f74cebc27a1a04
BossD3struction <BossD3struction@gmail.com>
2022-11-10 02:42:46 +0100

    ui update

--- resources/views/files/index.blade.php
+++ resources/views/files/index.blade.php
@@ -5,10 +5,10 @@
         <h1>Files page</h1>
         <form action="/found/files" method="POST" autocomplete="on">
             @csrf
-            <div class="input-group">
+            <div class="input-group input-group-lg">
                 <span class="input-group-text"><i class="bi bi-search"></i></span>
                 <input type="text" class="form-control" aria-label="Directory" placeholder="Enter directory path"
-                       value="D:\tmp_media_files_for_bachelor_project\Hudba\"
+                       value="D:\tmp_media_files_for_bachelor_project\"
                        name="directory" id="directory" autocomplete="off">
                 <button type="submit" class="btn btn-primary w-25">Search</button>
             </div>

3cd4172a75ee56ebd5ab177f9fa5052893195444
BossD3struction <BossD3struction@gmail.com>
2022-11-10 02:47:59 +0100

    new controller

---
+++ app/Http/Controllers/JpgFilesController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\DataTables\JpgFilesDataTable;
+use App\Models\JpgFile;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class JpgFilesController extends Controller
+{
+    public function renderJpgFilesTable(JpgFilesDataTable $dataTable)
+    {
+        return $dataTable->render('tables.jpg');
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param int $id
+     * @return Application|Factory|View
+     */
+    public function edit(int $id)
+    {
+        $file = JpgFile::find($id);
+        return view('files.jpg.edit', ['file' => $file]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param Request $request
+     * @param  int  $id
+     * @return Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+
+//    /**
+//     * Display a listing of the resource.
+//     *
+//     * @return Response
+//     */
+//    public function index()
+//    {
+//        //
+//    }
+//
+//    /**
+//     * Show the form for creating a new resource.
+//     *
+//     * @return Response
+//     */
+//    public function create()
+//    {
+//        //
+//    }
+//
+//    /**
+//     * Store a newly created resource in storage.
+//     *
+//     * @param Request $request
+//     * @return Response
+//     */
+//    public function store(Request $request)
+//    {
+//        //
+//    }
+//
+//    /**
+//     * Display the specified resource.
+//     *
+//     * @param  int  $id
+//     * @return Response
+//     */
+//    public function show($id)
+//    {
+//        //
+//    }
+}

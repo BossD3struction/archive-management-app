7eba741743a567ba2b644ab9431ba01b74c89e84
BossD3struction <BossD3struction@gmail.com>
2022-11-10 23:23:21 +0100

    new phpDocs, code cleanup

--- app/Http/Controllers/JpgFilesController.php
+++ app/Http/Controllers/JpgFilesController.php
@@ -12,6 +12,11 @@ use Illuminate\Http\Response;
 
 class JpgFilesController extends Controller
 {
+
+    /**
+     * @param JpgFilesDataTable $dataTable
+     * @return mixed
+     */
     public function renderJpgFilesTable(JpgFilesDataTable $dataTable)
     {
         return $dataTable->render('tables.jpg');
@@ -33,7 +38,7 @@ class JpgFilesController extends Controller
      * Update the specified resource in storage.
      *
      * @param Request $request
-     * @param  int  $id
+     * @param int $id
      * @return Response
      */
     public function update(Request $request, $id)
@@ -44,53 +49,11 @@ class JpgFilesController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param int $id
      * @return Response
      */
     public function destroy($id)
     {
         //
     }
-
-//    /**
-//     * Display a listing of the resource.
-//     *
-//     * @return Response
-//     */
-//    public function index()
-//    {
-//        //
-//    }
-//
-//    /**
-//     * Show the form for creating a new resource.
-//     *
-//     * @return Response
-//     */
-//    public function create()
-//    {
-//        //
-//    }
-//
-//    /**
-//     * Store a newly created resource in storage.
-//     *
-//     * @param Request $request
-//     * @return Response
-//     */
-//    public function store(Request $request)
-//    {
-//        //
-//    }
-//
-//    /**
-//     * Display the specified resource.
-//     *
-//     * @param  int  $id
-//     * @return Response
-//     */
-//    public function show($id)
-//    {
-//        //
-//    }
 }

c75c954a503dd388f45f5fd9688f8433dd9b8bb4
BossD3struction <BossD3struction@gmail.com>
2022-11-10 02:45:43 +0100

    jpg edit page

---
+++ resources/views/files/jpg/edit.blade.php
@@ -0,0 +1,82 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="text-black">
+        <div class="text-center text-uppercase fw-bold">
+            <h1>JPG file</h1>
+        </div>
+    </div>
+
+    <div class="form-group text-center mx-auto w-50 mb-3">
+        POG
+        {{--<form action="/files/mp3/{{ $file->id }}" method="POST">
+            @csrf
+            @method('PUT')
+            <div class="mb-3">
+                <label for="filename_path" class="fw-bold">FILENAME PATH</label>
+                <input type="text" class="form-control form-control-lg" name="filename_path" id="filename_path"
+                       value="{{ $file->filename_path }}" autocomplete="off" readonly>
+            </div>
+            <div class="mb-3">
+                <label for="filename" class="fw-bold">FILENAME</label>
+                <input type="text" class="form-control form-control-lg" name="filename" id="filename"
+                       value="{{ $file->filename }}" autocomplete="off" readonly>
+            </div>
+            <div class="mb-3">
+                <label for="title" class="fw-bold">TITLE</label>
+                <input type="text" class="form-control form-control-lg" name="title" id="title"
+                       value="{{ $file->title }}" autocomplete="off">
+            </div>
+            <div class="mb-3">
+                <label for="artist" class="fw-bold">ARTIST</label>
+                <input type="text" class="form-control form-control-lg" name="artist" id="artist"
+                       value="{{ $file->artist }}" autocomplete="off">
+            </div>
+            <div class="mb-3">
+                <label for="album" class="fw-bold">ALBUM</label>
+                <input type="text" class="form-control form-control-lg" name="album" id="album"
+                       value="{{ $file->album }}" autocomplete="off">
+            </div>
+            <div class="mb-3">
+                <label for="year" class="fw-bold">YEAR</label>
+                <input type="number" class="form-control form-control-lg" name="year" id="year" placeholder="YYYY"
+                       value="{{ substr($file->year, 0, 4) }}" autocomplete="off" min="1900" max="{{ date("Y") }}">
+            </div>
+            <div class="mb-3">
+                <label for="genres" class="fw-bold">GENRE</label>
+                <select class="form-select form-select-lg" name="genres" id="genres">
+                    @foreach($genres as $genre)
+                        @if($genre === $file->genre)
+                            <option selected value="{{ $genre }}">{{ $genre }}</option>
+                        @endif
+                        @if($genre !== $file->genre)
+                            <option value="{{ $genre }}">{{ $genre }}</option>
+                        @endif
+                    @endforeach
+                </select>
+            </div>
+            @if($errors->any())
+                <div class="alert-danger text-center mb-3 p-3">
+                    @foreach($errors->all() as $error)
+                        <li class="list-unstyled">
+                            {{ $error }}
+                        </li>
+                    @endforeach
+                </div>
+            @endif
+            @if(File::exists($file->filename_path))
+                <button class="btn btn-primary btn-lg w-50" type="submit">Update</button>
+            @endif
+            @if(File::missing($file->filename_path))
+                <button class="btn btn-primary btn-lg w-50" type="submit" disabled>Update</button>
+            @endif
+        </form>
+        <form action="/files/mp3/{{ $file->id }}" method="POST">
+            @csrf
+            @method('DELETE')
+            <button type="submit" class="btn btn-danger btn-lg w-50 mt-3">
+                Delete
+            </button>
+        </form>--}}
+    </div>
+@endsection

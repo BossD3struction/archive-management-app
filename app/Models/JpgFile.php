1571268f10afe96adb839809c1331ec45bae5f26
BossD3struction <BossD3struction@gmail.com>
2022-12-25 19:14:25 +0100

    code refactor

--- app/Models/JpgFile.php
+++ app/Models/JpgFile.php
@@ -2,19 +2,14 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 
 /**
- * @method static create(array $array)
- * @method static where(string $string, mixed $filenamePath)
- * @method static find(int $id)
- * @method static count()
+ * @mixin Builder
  */
 class JpgFile extends Model
 {
-    use HasFactory;
-
     protected $table = 'jpg_files';
     protected $primaryKey = 'id';
     protected $fillable = ['filename_path', 'filename', 'title', 'tags', 'comments', 'date', 'has_exif_metadata'];
--- app/Models/Mp3File.php
+++ app/Models/Mp3File.php
@@ -2,19 +2,14 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 
 /**
- * @method static create(array $array)
- * @method static where(string $string, int $id)
- * @method static find(int $id)
- * @method static count()
+ * @mixin Builder
  */
 class Mp3File extends Model
 {
-    use HasFactory;
-
     protected $table = 'mp3_files';
     protected $primaryKey = 'id';
     protected $fillable = ['filename_path', 'filename', 'title', 'artist', 'album', 'year', 'genre'];
